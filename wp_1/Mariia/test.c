e6fc4e851b7cae566a4ab351b79182a32225742e
mariiaz <mariiaz@student.chalmers.se>
2024-01-29 14:32:17 +0100

    test different algs

---
+++ wp_1/Mariia/test.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b);
+int compare(const void *a, const void *b);
+
+int main()
+{
+    // swap two numbers
+    int a = 5;
+    int b = 7;
+    printf("a = %i, b = %i\n", a, b);
+    printf("&a = %p, &b = %p\n", &a, &b);
+    swap(&a, &b);
+    printf("a = %i, b = %i\n", a, b);
+
+    // quick sort
+    int arr[] = {10, 5, 15, 12, 90, 80};
+    // gets number of elements in the array
+    // by dividing size of all elements in the arr by size of type stored in the arr
+    int n = sizeof(arr) / sizeof(arr[0]), i;
+    printf("%d, %lu, %lu\n", n, sizeof(arr), sizeof(arr[0]));
+
+    qsort(arr, n, sizeof(int), compare);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
