cb634a343dab18e936066339167da91b982fcaf1
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-01-30 02:50:14 +0100

    Add task_2 and task_5

--- wp_1/final/task_2.c
+++ wp_1/final/task_2.c
@@ -1,104 +1,219 @@
+// Include section
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_STR_LENGTH 100
+// Define section
+#define MAX_STR_LENGTH 100      // Define maximum length of the string to be encrypted
+#define NUMBER_OF_LETTERS 26    // Define the number of possible letters to be encrypted (In this case, the standard english alphabet)
 
+/**
+ * Helper function that checks if a character is UPPERCASE or lowercase
+ *
+ * Returns 1 if c is uppercase, 0 if c is lowercase, -1 if c is a non letter character
+ */
 int isCharUppercase(char c);
 
-int main(int argc, char** argv)
-{
-    char* userInput = malloc(MAX_STR_LENGTH);
+// Main program section
 
+/**
+ * This program encrypts a message by shifting the
+ * ASCII values of the characters
+ *
+ * Purpose: Practice ASCII manipulation
+ * DIT633
+ *
+ * Author: Ionel Pop, Joel Celen, Mariia Zabolotnia, 2024
+ *
+ **/
+int main(int argc, char **argv)
+{
+    // Variable declaration
+
+    // Declare a character array and allocate enough memory to store the user input
+    char *userInput = (char *)malloc((MAX_STR_LENGTH + 1) * sizeof(char));
+
+    // Check if the correct number of command line arguments was provided
     if (argc < 2)
     {
+        // Print error and return 1
         printf("Error! No encryption shift was provided\n");
         return 1;
     }
+    // Else if more than 2 arguments were provided
     else if (argc > 2)
     {
+        // Print error and return 1
         printf("Error! Too many arguments\n");
         return 1;
     }
 
-    int encryptionShift = atoi(argv[1]) % 26;
+    /**
+     * Get the encryption shift from the command line arguments.
+     * Use %26 because there are 26 letters.
+     *
+     * For example, if we get 30, encryptionShift will be 4
+     *
+     */
+    int encryptionShift = atoi(argv[1]) % NUMBER_OF_LETTERS; // Declare encryption shift
 
+    // "Infinite loop"
     while (1)
     {
-        char c;
-        int counter = 0;
+        char c;          // Declare a char c, to store current characters input by the user
+        int counter = 0; // Declare an int counter, to store the length of the string input by the user
+
+        // While the current character is different than `newline('\n')`
         while ((c = getchar()) != '\n')
         {
+            // If the current character is equal to EOF
             if (c == EOF)
             {
+                // Return 0 and print corresponding message and exit the program
                 printf("EOF Detected. Exiting the program\n");
                 return 0;
             }
-            // Else if, check if alpha and not a space
+            // Else if character is not a letter nor a space
             else if (isCharUppercase(c) == -1 && c != ' ')
             {
+                // Return 0 and print corresponding message and exit the program
                 printf("Error. Non alpha character nor space entered\n");
                 return 0;
             }
-            userInput[counter] = c;
-            counter++;
+
+            /**
+             * Append the current char to the userInput string
+             * Also, update the counter to keep track of the length
+             */
+            userInput[counter++] = c;
         }
+
+        // When the user input has been processed, append the string terminator character at the end
         userInput[counter] = '\0';
 
+        // Iterate through the string that store the user input
         for (int i = 0; i < strlen(userInput); i++)
         {
-            int encrypted = 0;
+            int encrypted = 0; // Variable to keep track of the resulting encrypted character
+
+            // If the character is uppercase
             if (isCharUppercase(userInput[i]) == 1)
             {
-                // Z + 13 -> 90 + 13 = 103 -> should not be g, it should be 65 + 13 - 1 -> 77 -> M
+                /**
+                 * In this case, we don't want our encryption to go into the lowercase letters.
+                 *
+                 * For example, if we are encrypting Z and encryptionShift is 13:
+                 *      'Z' + 13 -> 90 + 13 = 103 -> But 103 is 'g'
+                 *      In contrast, the result should be
+                 *      65 + (103 % 90) - 1  = 77 -> Which is 'M', the proper result
+                 *
+                 * We also don't want our encryption to go bellow bounds when having negative shifts.
+                 *
+                 * For example, if we are encrypting B and encryptionShift is -3:
+                 *      'B' - 3 -> 66 - 3 = 63 -> Which is bellow 'A' (65)
+                 *      Therefore, we calculate the correct char as it follows:
+                 *      encrypted = 90 ('Z') - (65 ('A') - encrypted) + 1;
+                 *      Which will give us 98 -> 'Y'
+                 */
+
+                // Add the ASCII value of the current character in the user input + the encryption shift
                 encrypted = ((int)userInput[i] + encryptionShift);
 
-                encrypted = encrypted <= 90 ? encrypted : 65 + (((int)userInput[i] + encryptionShift) % 90) - 1;
+                // If the encrypted char goes above bounds
+                if (encrypted > 'Z')
+                {
+                    // Do what it is explained above.
+                    encrypted = 'A' + (((int)userInput[i] + encryptionShift) % 'Z') - 1;
+                }
+                // Else if the encrypted char goes bellow bounds
+                else if (encrypted < 'A')
+                {
+                    // Do what it is explained above
+                    encrypted = 'Z' - ('A' - encrypted) + 1;
+                }
             }
-            // Input is lowercase
+            // Else if the character is lowercase
             else if (isCharUppercase(userInput[i]) == 0)
             {
-                // z + 13 -> 122 + 13 = 135 -> it should be 109 -> 135 -> M
+                /**
+                 * Similarly to the previous case, we don't want our encryption to go above bounds.
+                 *
+                 * For example, if we are encrypting z and encryptionShift is 13:
+                 *      'Z' + 13 -> 122 + 13 = 135 -> But it should be 109 which is 'm'
+                 *      97 + (135 % 122) - 1  = 109 -> Which is 'm', the proper result
+                 */
+
+                // Add the ASCII value of the current character in the user input + the encryption shift
                 encrypted = ((int)userInput[i] + encryptionShift);
 
-                encrypted = encrypted <= 122 ? encrypted : 97 + (((int)userInput[i] + encryptionShift) % 122) - 1;
+                // If the encrypted char goes above bounds
+                if (encrypted > 'z')
+                {
+                    // Do what it is explained above.
+                    encrypted = 'a' + (((int)userInput[i] + encryptionShift) % 'z') - 1;
+                }
+                // Else if the encrypted char goes bellow bounds
+                else if (encrypted < 'a')
+                {
+                    // Do what it is explained above
+                    encrypted = 'z' - ('a' - encrypted) + 1;
+                }
             }
+
+            /**
+             * Else if the current character in the input is a space.
+             * 
+             * This allows us to encrypt sentences instead of only words
+             */
             else if (userInput[i] == ' ')
             {
+                // We assign the encrypted character to a space
                 encrypted = 32;
             }
 
+            // If the character could not be encrypted
             if (encrypted == 0)
             {
+                // Print an error message and finish the program
                 printf("Oops. Something went wrong :(\n");
                 return 0;
             }
 
+            // If everything worked properly, print the encrypted character (or a space)
             printf("%c", encrypted);
         }
 
+        // Print the end of line for the encrypted string
         printf("\n");
     }
     return 0;
 }
 
-int isCharUppercase(char c)
+int isCharUppercase(char c) // Helper function defined in the beginning
 {
+    // If the character is between 'A' and 'Z' (65 and 90)
     if (c <= 'Z' && c >= 'A')
     {
+        // Return 1: It is UPPERCASE
         return 1;
     }
+    // Else if the character is between 'a' and 'z' (97 and 122)
     else if (c <= 'z' && c >= 'a')
     {
+        // Return 0: It is lowercase
         return 0;
     }
+    // Otherwise, if the character is not a letter
     else
     {
+        // Return -1
         return -1;
     }
 }
 
 /*
+Initial idea:
+
 If it's lower case -> ASCII -> [97, 122]
 
 To get the conversion, it's just the current ascii + conversion
--- wp_1/final/task_5.c
+++ wp_1/final/task_5.c
@@ -1,101 +1,151 @@
+// Include section
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 // TODO: ask why it doesn't give me an error if freq[10] doesn't exist
 // TODO: ask if I can modify the signatures
+// TODO: ask if it's ok to remove 'n'
 
-// #define MAX 100         // Defines the maximum number of the values in the table
+// #define MAX 100         // Defines the maximum number of values in the table
 // #define MAXNUMBER 20    // Defines the maximum value of random numbers
 
 // ------ Function declarations ----------
 
 // This function generates a set of random numbers
 // and fills the table *tab with these numbers
-void create_random(int *tab, int n, int MAXNUMBER);
+void create_random(int *tab, int n, int maxNumber);
 
 // This function takes the *tab of random numbers
 // and creates a table with the frequency counts for these numbers
-void count_frequency(int *tab, int *freq, int n, int MAXNUMBER);
+void count_frequency(int *tab, int *freq, int n, int maxNumber);
 
 // This function takes the frequency count table
 // and draws a histogram of the values in that frequency table
-void draw_histogram(int *freq, int MAXNUMBER);
+void draw_histogram(int *freq, int maxNumber);
 
-// TEST
+// This function prints an array
 void print_array(int *arr, int n);
-
-// ------ Function definitions ----------
 
 // ------ Main --------------------------
 
-// The main entry point for the program
-//
-// If you choose to go for the optional part
-// Please modify it accordingly
+/**
+ * This program generates a random integer array,
+ * counts the frequency of each number in the array,
+ * and prints an histogram with the values
+ *
+ * Purpose: Pointers
+ * DIT633
+ *
+ * Author: Ionel Pop, Joel Celen, Mariia Zabolotnia, 2024
+ *
+ **/
 int main(int argc, char** argv)
 {
-    if (argc < 2)
+    // If the provided command line arguments are not enough
+    if (argc < 3)
     {
-        printf("Error! Not enough arguments");
-        return 1;
+        // If the command line arguments are 2
+        if (argc == 2)
+        {
+            // Check if the command line argument provided is the flag `-h`
+            if (strcmp(argv[1], "-h") == 0)
+            {
+                // Print instructions for the program
+                printf("To use this program provide:\n\t- the `maximum number of values`,and\n\t- the `maximum value`\nas command line arguments\n");
+                return 0;
+            }
+        }
+        // Otherwise, print an appropiate error message and return 0
+        printf("Error! Not enough arguments\n");
+        printf("Provide flag -h to display instructions\n");
+        return 0;
     }
+
+    // Generate a seed based on the current time to use with the random function
     srand(time(0));
 
-    int MAX = atoi(argv[1]);
-    int MAXNUMBER = atoi(argv[2]);
+    int maxValues = atoi(argv[1]); // Maximum number of values from the command line arguments
+    int maxNumber = atoi(argv[2]); // Maximum value from the command line arguments
 
-    int table[MAX], n;
-    int frequency[MAXNUMBER];
+    // If one of those values is 0
+    if (maxValues == 0 || maxNumber == 0)
+    {
+        // Print an appropiate error message and return 1
+        printf("Error! Can't provide '0' as argument\n");
+        return 1;
+    }
 
-    n = MAX;
+    int table[maxValues]; // Initialize an array of maxValues integers
+    int frequency[maxNumber]; // Initialize an array of maxNumber integers
 
-    create_random(table, n, MAXNUMBER);
-    print_array(table, MAX);
+    // Call the function create_random
+    create_random(table, maxValues, maxNumber);
+    // Call a  helper function to print the table array
+    print_array(table, maxValues);
 
-    count_frequency(table, frequency, n, MAXNUMBER);
-    print_array(frequency, MAXNUMBER);
+    // Call the function count_frequency
+    count_frequency(table, frequency, maxValues, maxNumber);
+    // Call a  helper function to print the table array
+    print_array(frequency, maxNumber);
 
-    draw_histogram(frequency, MAXNUMBER);
+    // Call the function draw_histogram
+    draw_histogram(frequency, maxNumber);
 }
 
-void create_random(int *tab, int n, int MAXNUMBER)
+// ------ Function definitions ----------
+
+void create_random(int *tab, int n, int maxNumber)
 {
+    // Iterate from 0 to n
     for (int i = 0; i < n; i++)
     {
-        tab[i] = rand() % (MAXNUMBER);
+        // Populate the current location in the array with a random number between 0 and maxNumber
+        tab[i] = rand() % (maxNumber);
     }
 }
 
-void count_frequency(int *tab, int *freq, int n, int MAXNUMBER)
+void count_frequency(int *tab, int *freq, int n, int maxNumber)
 {
-    for (int i = 0; i < MAXNUMBER; i++)
+    // Iterate from 0 to maxNumber
+    for (int i = 0; i < maxNumber; i++)
     {
         // Initialize the array to {0, 0, 0, ..., 0}
         freq[i] = 0;
     }
 
+    // Iterate from 0 to n
     for (int i = 0; i < n; i++)
     {
+        // Get the current number from the table
         int currentNumber = tab[i];
+
+        // Update the counter for that number in the frequency table
         freq[currentNumber] += 1;
     }
 }
 
-void draw_histogram(int *freq, int MAXNUMBER)
+void draw_histogram(int *freq, int maxNumber)
 {
     printf("Printing histogram...\n");
-    for (int i = 0; i < MAXNUMBER; i++)
+    // Iterate from 0 to maxNumber in the frequency table
+    for (int i = 0; i < maxNumber; i++)
     {
-        if (freq[i] != 0)
+        // If the frequency count for the current number in the frequency table is more than 0
+        if (freq[i] > 0)
         {
+            // Print the number
             printf("%d\t", i);
 
+            // Iterate from 0 to the frequency count
             for (int j = 0; j < freq[i]; j++)
             {
+                // Print the corresponding number of Xs to the frequency
                 printf("X");
             }
 
+            // Print end of line to account for proper formatting
             printf("\n");
         }
     }
@@ -106,10 +156,13 @@ void print_array(int *arr, int n)
 {
     printf("Printing array...\n");
     printf("[");
+    // Iterate through the array
     for (int i = 0; i < n - 1; i++)
     {
+        // Print the data in the array
         printf("%d, ", arr[i]);
     }
+    // Print the last element of the array + formatting
     printf("%d]\n", arr[n - 1]);
     printf("That was the array :)\n\n");
 }
