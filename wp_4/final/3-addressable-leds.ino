340e3adc9feb24c4ce5f000672d7e80362ffe660
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-02-20 14:40:37 +0100

    final

---
+++ wp_4/final/3-addressable-leds.ino
@@ -0,0 +1,202 @@
+// (C) __Mariia Zabolotnia, Joel Celén, Ionel Pop, group: 8__ (2024)
+// Work package 4
+// Exercise 3
+// Submission code: 022008 (provided by your TA-s)
+
+// ---- Source code for interrupt timer ----
+
+// AVR Timer CTC Interrupts Calculator
+// v. 8
+// http://www.arduinoslovakia.eu/application/timer-calculator
+// Microcontroller: ATmega328P
+// Created: 2024-02-18T11:17:54.307Z
+
+// ------------------------------------------
+
+// Include section
+#include <Adafruit_NeoPixel.h>
+
+// Function prototypes
+void setupTimer1();     // Function to set up timed interrupts
+ISR(TIMER1_COMPA_vect); // Interrupt function
+float convertCelsius(); // Function that converts analog value to celsius
+void setRing();         // Function that sets all the LEDs in the NeoPixel ring
+void setLeds();         // Function that sets boundary LEDs
+
+// Define section
+#define TEMP_SENSOR A0
+#define PIXEL_RING 6   // Pin connected to the NeoPixel ring
+#define NUM_LEDS 12    // Number of NeoPixels in the ring
+#define BRIGHTNESS 100 // NeoPixel brightness (0-255)
+#define MAX_LED 13     // Pin for max value LED
+#define MIN_LED 10     // Pin for min value LED
+
+// Global varibles
+volatile int tempValue = 0;                                            // Variable for analog reading from temp sensor
+volatile int newReading = 0;                                           // Flag for when a new reading is available
+Adafruit_NeoPixel ledRing(NUM_LEDS, PIXEL_RING, NEO_GRB + NEO_KHZ800); // NeoPixel ring declaration
+float min_range = -30.0;                                               // Minimum value of temp range
+float max_range = 80.0;                                                // Maximum value of temp range
+
+// 2D array that holds the RGB values matching each LED
+int colorScale[NUM_LEDS][3] = {
+    {0, 255, 0},   // Green
+    {85, 255, 0},  // Light green
+    {170, 255, 0}, // Light yellow-green
+    {255, 255, 0}, // Yellow-green
+    {255, 170, 0}, // Yellow
+    {255, 85, 0},  // Orange-yellow
+    {255, 0, 0},   // Orange
+    {255, 0, 85},  // Orange-red
+    {255, 0, 170}, // Red-orange
+    {255, 0, 255}, // Red
+    {170, 0, 255}, // Deep red
+    {85, 0, 255}   // Dark red
+};
+
+// Arduino setup function
+void setup()
+{
+  setupTimer1();                     // Call the function to set up Timer2
+  Serial.begin(9600);                // Start serial
+  ledRing.begin();                   // Initialize the NeoPixel ring
+  ledRing.setBrightness(BRIGHTNESS); // Set brightness
+  ledRing.show();                    // Initialize initial LED values
+  pinMode(MAX_LED, OUTPUT);          // Assign pinMode to max led
+  pinMode(MIN_LED, OUTPUT);          // Assign pinMode to min led
+}
+
+// Loop function
+void loop()
+{
+  // If there is a new reading
+  if (newReading == 1)
+  {
+    // Set the LEDs in the NeoPixel
+    setRing();
+    // Set threshold LEDs
+    setLeds();
+    // Set flag to false
+    newReading = 0;
+  }
+}
+
+// Interrupt function skeleton:
+// https://www.arduinoslovakia.eu/application/timer-calculator
+// Code defined within the function is out original work.
+ISR(TIMER1_COMPA_vect)
+{
+  // Read the value from the temp sensor
+  tempValue = analogRead(TEMP_SENSOR);
+  // Set flag for new reading to true
+  newReading = 1;
+}
+
+// Function to set the LEDs in the NeoPixel ring according to the temp values
+void setRing()
+{
+  // Convert analog value to celsius and store in a variable
+  float celsius = convertCelsius();
+
+  // Set threshold to min_range
+  float threshold = min_range;
+
+  // Turn on NeoPixel LEDs based on different values for each LED
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    // If current temperature reading is higher than threshold
+    if (celsius > threshold)
+    {
+      // Light up LED at i with the corresponding RBG value at i
+      ledRing.setPixelColor(i, colorScale[i][0], colorScale[i][1], colorScale[i][2]);
+    }
+    // If current temperature reading is below threshold
+    else
+    {
+      // Turn of LED at i
+      ledRing.setPixelColor(i, 0, 0, 0); // Turn off pixel
+    }
+    // Display changes in LEDs
+    ledRing.show();
+    // Increment threshold by 10c for LED at i+1
+    threshold += 10.0;
+  }
+}
+
+// Function that sets threshold LEDs based on current temp reading
+void setLeds()
+{
+  // Convert analog value to celsius and store in a variable
+  float celsius = convertCelsius();
+
+  digitalWrite(MIN_LED, LOW); // Turn off min threshold LED
+  digitalWrite(MAX_LED, LOW); // Turn off max threshold LED
+
+  // If current temperature is below min threshold value
+  if (celsius < min_range)
+  {
+    // Light up the min value led
+    digitalWrite(MIN_LED, HIGH);
+  }
+  // If current temperature is above max threshold value
+  else if (celsius > max_range)
+  {
+    // Light up the max value led
+    digitalWrite(MAX_LED, HIGH);
+  }
+}
+
+// Function to read temperature from a temperature sensor and convert to Celsius
+float convertCelsius()
+{
+
+  // Convert the raw ADC value to voltage
+  float voltage = tempValue * (5.0 / 1023.0);
+
+  // Convert the voltage to celsius
+  float temperatureCelsius = (voltage - 0.5) * 100.0;
+
+  // Return value in celsius
+  return temperatureCelsius;
+}
+
+// Setup for timed interrupt, source code from:
+// https://www.arduinoslovakia.eu/application/timer-calculator
+void setupTimer1()
+{
+  noInterrupts();
+  // Clear registers
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+
+  // 1 Hz (16000000/((15624+1)*1024))
+  OCR1A = 15624;
+
+  // 2 Hz (16000000/((31249+1)*256))
+  // OCR1A = 31249;
+
+  // 3 Hz (16000000/((20832+1)*256))
+  // OCR1A = 20832;
+
+  // 4 Hz (16000000/((15624+1)*256))
+  // OCR1A = 15624;
+
+  // 5 Hz (16000000/((3124+1)*1024))
+  // OCR1A = 3124;
+
+  // 10 Hz (16000000/((6249+1)*256))
+  // OCR1A = 6249;
+
+  // CTC
+  TCCR1B |= (1 << WGM12);
+
+  // Prescaler 1024
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+
+  // Output Compare Match A Interrupt Enable
+  TIMSK1 |= (1 << OCIE1A);
+
+  // Re-enable interrupts
+  interrupts();
+}
---
+++ wp_4/final/4-lists.c
@@ -0,0 +1,128 @@
+// (C) __Mariia Zabolotnia, Joel Celén, Ionel Pop, group: 8__ (2024)
+// Work package 4
+// Exercise 4
+// Submission code: 022008 (provided by your TA-s)
+
+// Include section
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// function finds the number in the array and returns its index, or returns -1 if the num is not in the array
+int search_number(int number, int tab[], int size);
+// function sorts array in place utilizing bubble sort algorithm
+void bubble_sort(int number, int tab[]); // bubble sort
+// function sorts array in place utilizing selection sort algorithm
+void selection_sort(int number, int tab[]); // selection sort
+// function to swap two elements, takes pointers to the elements to be swapped
+void swap(int *a, int *b);
+
+// main function declaration
+int main(int argc, char *argv[])
+{
+    // declare array to store items we will be iterating over
+    int test[] = {1, 2, 34, 5, 67, 3, 23, 12, 13, 10};
+    // declare a variable to store item we are looking for
+    int num_to_find = 3;
+    // finding number of elements in the array by dividing size of the array by the size of every element
+    int array_size = sizeof(test) / sizeof(test[0]);
+    // execute function to get number index
+    int result = search_number(num_to_find, test, array_size);
+    // print the message depending on the result
+    result == -1 ? printf("Didn't find number %d in the array\n", num_to_find) : printf("Index of number %d is: %d\n", num_to_find, result);
+
+    // execute function to sort the array using bubble_sort
+    // bubble_sort(array_size, test);
+
+    // execute function to sort the array using bubble_sort
+    selection_sort(array_size, test);
+
+    // print user message
+    printf("Sorted array: \n");
+    // iterate over all elements of the sorted array
+    for (int i = 0; i < array_size; i++)
+    {
+        // print all elements of the sorted array
+        printf("%d ", test[i]);
+    }
+    printf("\n");
+    // terminate the program
+    return 1;
+}
+
+// function finds the number in the array and returns its index, or returns -1 if the num is not in the array
+int search_number(int number, int tab[], int size)
+{
+    // initialize pointer to the first element of the array
+    int *ptr = tab;
+    // while pointer doesn't reach the address of the last element in the array search for the number
+    while (ptr < &tab[size - 1])
+    {
+
+        // if the value of the cell pointer is pointing to is equal to the number we are looking for
+        if (*ptr == number)
+        {
+            // return the index of the element
+            return ptr - tab;
+        }
+        // move pointer to the next element
+        ptr++;
+    }
+    // if n is not in the array, then return -1
+    return -1;
+}
+
+// function that sorts the array in place
+void bubble_sort(int number, int *tab)
+{
+    // loop over the elements starting with 0. for the current element we need to check if there are any items that are smaller that curr
+    for (int i = 0; i < number - 1; i++)
+    {
+        // check the items in the right part of the array and see if there are any that are less than the current one
+        for (int j = 0; j < number - i - 1; j++)
+        {
+            // if we found element that is smaller
+            // we are using pointer to the first element of the array and j as a counter to how many positions to shift pointer in the array
+            if (*(tab + j) > *(tab + j + 1))
+            {
+                // swap current and found element
+                swap((tab + j), (tab + j + 1));
+            }
+        }
+    }
+}
+
+// function that sorts the array in place using selection sort
+void selection_sort(int number, int tab[])
+{
+    // for every item in the array, execute the following
+    for (int i = 0; i < number - 1; i++)
+    {
+        // initialize minimum as the first element of the array
+        int min = i;
+        // then repetitively check every other item in the array to find the new min
+        for (int j = i + 1; j < number; j++)
+        {
+            // if the element is less than the current minimum
+            if (tab[j] < tab[min])
+            {
+                // assign the new minimum
+                min = j;
+            }
+        }
+        // swap current item and the found minimum item
+        swap(&tab[min], &tab[i]);
+    }
+}
+
+// function to swap two items, takes pointers to the items to be able to pass them as pass-by-reference and swap
+void swap(int *a, int *b)
+{
+    // store value of the first item in temp
+    int temp = *a;
+    // assign value of the second one to the first one
+    *a = *b;
+    // assign value of the temp to the second one
+    *b = temp;
+}
